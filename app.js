954e6fcb2310167575f6f46c759aeb583e4c3142
Als <malkautsars@gmail.com>
2019-06-26 17:03:37 +0800

    Express Configuration + Router

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send("Hello JS");
-})
+const mysql = require('mysql')
+const myConnection = require('express-myconnection')
+
+const dbConfig = require('./dbConfig')
+const dbOptions = {
+    host: dbConfig.database.host,
+    user: dbConfig.database.user,
+    password: dbConfig.database.password,
+    port: dbConfig.database.port,
+    database: dbConfig.database.database
+}
+
+app.use(myConnection(mysql, dbOptions, 'pool'))
+
+app.set('view engine', 'ejs')
+app.use(express.static("views"))
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+const imgUpload = require('./routes/imgupload')
+app.use('/', imgUpload)
 
 app.listen(3000, () => {
     console.log('Server running at port 3000: http://127.0.0.1:3000')
---
+++ dbconfig.js
@@ -0,0 +1,11 @@
+var config = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: 'Sesar181196',
+        port: 3306,
+        database: 'imageupload'
+    }
+}
+
+module.exports = config;

f310c6081fcc7ab9e1a03ea1a737b4cb7144802d
Als <malkautsars@gmail.com>
2019-10-15 23:32:51 +0800

    feat: reCAPTCHA

--- app.js
+++ app.js
@@ -19,11 +19,14 @@ app.set('view engine', 'ejs')
 app.use(express.static("views"))
 
 var bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(bodyParser.json())
 
 const imgUpload = require('./routes/imgupload')
 app.use('/', imgUpload)
+
 
 app.listen(3000, () => {
     console.log('Server running at port 3000: http://127.0.0.1:3000')
--- routes/imgupload.js
+++ routes/imgupload.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const request = require('request');
 
 // Multer
 const multer = require('multer')
@@ -7,7 +8,6 @@ const dir = './uploads'
 
 // Sharp
 const sharp = require('sharp');
-
 const path = require('path')
 
 let storage = multer.diskStorage({
@@ -24,41 +24,77 @@ let upload = multer({
 });
 
 message = ''
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     res.render('index', message)
 })
 
 app.post('/upload', upload.single('profile'), async (req, res) => {
-    if (!req.file) {
-        message = "Error! in image upload."
+    const captchaKey = req.body['g-recaptcha-response'];
+    if (
+        req.body['g-recaptcha-response'] === undefined ||
+        req.body['g-recaptcha-response'] === '' ||
+        req.body['g-recaptcha-response'] === null
+    ) {
+        message = "Error! Please Select Captcha."
         res.render('index', {
             message: message,
             status: 'danger'
         });
-    } else {
-        // Resize
-        sharp(req.file.path).toBuffer().then((data) => {
-            sharp(data).resize(800).toFile(req.file.path, (err, info) => {
-                // Data
-                var imageData = {
-                    name: req.file.filename,
-                    type: req.file.mimetype,
-                    size: req.file.size
-                }
-                req.getConnection(function (err, con) {
-                    con.query('INSERT INTO imgFile SET ?', imageData, function (err, result) {
-                        message = "Successfully! uploaded";
-                        res.render('index', {
-                            message: message,
-                            status: 'success'
-                        });
-                    });
-                })
-            })
-        }).catch((err) => {
-            console.log(err)
-        })
     }
+
+    // reCAPTCHA SECRET KEY
+    // 6LdaSb0UAAAAAEDR9g2oKo7HU5AHz4MqDbPeZU0o
+
+    // Secret Key
+    const secretKey = '6LdaSb0UAAAAAEDR9g2oKo7HU5AHz4MqDbPeZU0o';
+
+    // Verify URL
+    const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captchaKey}&remoteip=${req.connection.remoteAddress}`;
+
+    // Make Request To VerifyURL
+    request(verifyUrl, (err, response, body) => {
+        body = JSON.parse(body);
+
+        // If Not Successful
+        if (body.success !== undefined && !body.success) {
+            message = "Error! Failed captcha verification."
+            res.render('index', {
+                message: message,
+                status: 'danger'
+            });
+        } else {
+            if (!req.file) {
+                message = "Error! in image upload."
+                res.render('index', {
+                    message: message,
+                    status: 'danger'
+                });
+            } else {
+                // Resize
+                sharp(req.file.path).toBuffer().then((data) => {
+                    sharp(data).resize(800).toFile(req.file.path, (err, info) => {
+                        // Data
+                        var imageData = {
+                            name: req.file.filename,
+                            type: req.file.mimetype,
+                            size: req.file.size
+                        }
+                        req.getConnection(function (err, con) {
+                            con.query('INSERT INTO imgFile SET ?', imageData, function (err, result) {
+                                message = "Successfully! uploaded";
+                                res.render('index', {
+                                    message: message,
+                                    status: 'success'
+                                });
+                            });
+                        })
+                    })
+                }).catch((err) => {
+                    console.log(err)
+                })
+            }
+        }
+    });
 })
 
 module.exports = app;
